388e2c146889587efe455ad020cb70e2e4ee2e64
RatnaIndahA <ratna734@gmail.com>
2025-03-14 11:53:53 +0700

    acara 22

--- laravelproject_web4/app/Http/Controllers/Backend/ApiPendidikanController.php
+++ laravelproject_web4/app/Http/Controllers/Backend/ApiPendidikanController.php
@@ -14,4 +14,49 @@ class ApiPendidikanController extends Controller
         $pendidikan = Pendidikan::all();
         return Response::json($pendidikan, 201);
     }
+    public function getPen($id)
+    {
+    $pendidikan = Pendidikan::find($id);
+
+    return Response::json($pendidikan, 200);
+    }
+    public function createPen(Request $request)
+    {
+    Pendidikan::create($request->all());
+
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'Pendidikan berhasil ditambahkan!'
+    ], 201);
+    }
+    public function updatePen($id, Request $request)
+    {
+    $pendidikan = Pendidikan::find($id);
+    if (!$pendidikan) {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Data pendidikan tidak ditemukan!'
+        ], 404);
+    }
+    $pendidikan->update($request->all());
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'Pendidikan berhasil dirubah!'
+    ], 200);
+    }
+    public function deletePen($id)
+    {
+    $pendidikan = Pendidikan::find($id);
+    if (!$pendidikan) {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Data pendidikan tidak ditemukan!'
+        ], 404);
+    }
+    $pendidikan->delete();
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'Pendidikan berhasil dihapus!'
+    ], 200);
+    }
 }
